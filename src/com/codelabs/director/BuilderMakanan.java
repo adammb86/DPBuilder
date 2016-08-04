36cb5b89bcfcd573dca68e60ee990ca48f372550
adammb86 <adammbachtiar@gmail.com>
2016-08-04 14:53:11 +0700

    "Menambahkan perulangan untuk create objek di BuilderMakanan"

--- src/com/codelabs/director/BuilderMakanan.java
+++ src/com/codelabs/director/BuilderMakanan.java
@@ -1,8 +1,11 @@
 package com.codelabs.director;
 
 import com.codelabs.product.Makanan;
+import com.codelabs.realization.BurgerAyam;
 import com.codelabs.realization.BurgerSayuran;
 import com.codelabs.realization.TehBohay;
+import com.codelabs.realization.TehSusu;
+
 
 /**
  * Created by adammb on 7/28/16.
@@ -10,6 +13,8 @@ import com.codelabs.realization.TehBohay;
 public class BuilderMakanan {
     //persiapkan method sejumlah paket makanan yang ditawarkan
     //kalau mau custom makanan dan minuman kirim via parameter
+
+
 
     public Makanan siapkanMakananSayur(){
         Makanan makanan=new Makanan();
@@ -19,19 +24,30 @@ public class BuilderMakanan {
         return makanan;
     }
 
-    public Makanan siapkanMakananCustom(String namaMakanan, String namaMinuman){
-        Makanan menu=new Makanan();
-        if(namaMakanan.equalsIgnoreCase("burger sayuran"))
-            menu.addItem(new BurgerSayuran());
-        else if(namaMakanan.equalsIgnoreCase("burger ayam"))
-            menu.addItem(new BurgerAyam());
-        else
-            return null;
+    public Makanan siapkanMakananCustom(String namaMakanan, int jumlahMakanan, String namaMinuman, int jumlahMinuman){
+        Makanan makanan=new Makanan();
 
-        if(namaMakanan.equalsIgnoreCase("burger sayuran"))
-            menu.addItem(new BurgerSayuran());
-        else if(namaMakanan.equalsIgnoreCase("burger ayam"))
-            menu.addItem(new BurgerAyam());
+        if(namaMakanan.equalsIgnoreCase("burgerayam")) {
+            for (int i = 1; i <= jumlahMakanan; i++)
+                makanan.addItem(new BurgerAyam());
+        }
+        else if(namaMakanan.equalsIgnoreCase("burgersayuran")){
+            for (int i = 1; i <= jumlahMakanan; i++)
+                makanan.addItem(new BurgerSayuran());
+        }
+
+        if(namaMinuman.equalsIgnoreCase("tehbohay")){
+            for (int i = 1; i <= jumlahMakanan; i++)
+                makanan.addItem(new TehBohay());
+        }
+        else if(namaMinuman.equalsIgnoreCase("tehsusu")){
+            for (int i = 1; i <= jumlahMakanan; i++)
+                makanan.addItem(new TehSusu());
+        }
+
+        //untuk memeriksa apakah ada makanan atau minuman yang sesuai dengan pesanan
+        if(makanan.jumlahItem()>0)
+            return makanan;
         else
             return null;
     }
